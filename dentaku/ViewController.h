df190ec4f6a59f65c745178d4649a0d5f2f3c0cd
iwata kento <2011531005@ma224.is.env.kitakyu-u.ac.jp>
2013-11-14 15:52:11 +0900

    ボタン接続

--- dentaku/ViewController.h
+++ dentaku/ViewController.h
@@ -6,8 +6,50 @@
 //  Copyright (c) 2013年 univercity of kitakyusyu. All rights reserved.
 //
 
+
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
 
+@interface ViewController : UIViewController{
+    IBOutlet UILabel *result;
+    IBOutlet UIButton *one;
+    IBOutlet UIButton *two;
+    IBOutlet UIButton *three;
+    IBOutlet UIButton *four;
+    IBOutlet UIButton *five;
+    IBOutlet UIButton *six;
+    IBOutlet UIButton *seven;
+    IBOutlet UIButton *eight;
+    IBOutlet UIButton *nine;
+    IBOutlet UIButton *zero;
+    IBOutlet UIButton *plus;
+    IBOutlet UIButton *minus;
+    IBOutlet UIButton *multiply;
+    IBOutlet UIButton *divide;
+    IBOutlet UIButton *equal;
+    IBOutlet UIButton *clear;
+    IBOutlet UIButton *dot;
+    
+    
+}
+
+
+-(IBAction) one_down:(id)sender;
+-(IBAction) two_down:(id)sender;
+-(IBAction) three_down:(id)sender;
+-(IBAction) four_down:(id)sender;
+-(IBAction) five_down:(id)sender;
+-(IBAction) six_down:(id)sender;
+-(IBAction) seven_down:(id)sender;
+-(IBAction) eight_down:(id)sender;
+-(IBAction) nine_down:(id)sender;
+-(IBAction) zero_down:(id)sender;
+-(IBAction) plus_down:(id)sender;
+-(IBAction) minus_down:(id)sender;
+-(IBAction) multiply_down:(id)sender;
+-(IBAction) devide_down:(id)sender;
+-(IBAction) equal_down:(id)sender;
+-(IBAction) clear_down:(id)sender;
+-(IBAction) dot_down:(id)sender;
 @end
+
