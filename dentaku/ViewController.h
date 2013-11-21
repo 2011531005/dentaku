f79c0a9758865f3cb8121151c283e6ff842ce655
iwata kento <2011531005@ma224.is.env.kitakyu-u.ac.jp>
2013-11-21 16:02:51 +0900

    tag

--- dentaku/ViewController.h
+++ dentaku/ViewController.h
@@ -11,17 +11,14 @@
 
 
 @interface ViewController : UIViewController{
+    BOOL startInput;
+    float currentValue;
+    int operation;
     IBOutlet UILabel *result;
-    IBOutlet UIButton *one;
-    IBOutlet UIButton *two;
-    IBOutlet UIButton *three;
-    IBOutlet UIButton *four;
-    IBOutlet UIButton *five;
-    IBOutlet UIButton *six;
-    IBOutlet UIButton *seven;
-    IBOutlet UIButton *eight;
-    IBOutlet UIButton *nine;
-    IBOutlet UIButton *zero;
+    
+    IBOutletCollection(UIButton) NSArray *numberButtons;
+    
+    
     IBOutlet UIButton *plus;
     IBOutlet UIButton *minus;
     IBOutlet UIButton *multiply;
@@ -32,18 +29,11 @@
     
     
 }
+@property (retain,nonatomic)IBOutlet UILabel *LabelText;
 
 
--(IBAction) one_down:(id)sender;
--(IBAction) two_down:(id)sender;
--(IBAction) three_down:(id)sender;
--(IBAction) four_down:(id)sender;
--(IBAction) five_down:(id)sender;
--(IBAction) six_down:(id)sender;
--(IBAction) seven_down:(id)sender;
--(IBAction) eight_down:(id)sender;
--(IBAction) nine_down:(id)sender;
--(IBAction) zero_down:(id)sender;
+- (IBAction)pushNumber:(id)sender;
+
 -(IBAction) plus_down:(id)sender;
 -(IBAction) minus_down:(id)sender;
 -(IBAction) multiply_down:(id)sender;
--- dentaku/ViewController.m
+++ dentaku/ViewController.m
@@ -18,12 +18,35 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    NSEnumerator *enumerator = [numberButtons objectEnumerator];
+    id anObject;
+    
+    while (anObject = [enumerator nextObject]) {
+        UIButton *button = (UIButton*)anObject;
+        button.tag = [button.titleLabel.text intValue];
+    }
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    
+   }
+
+- (IBAction)pushNumber:(id)sender {
+    UIButton *button = (UIButton*)sender;
+    //NSLog(@"Button.tag = %d", button.tag);
+    if(button.tag==0){
+        _LabelText.text = [NSString stringWithFormat:@"d",button.tag];
+        startInput=NO;
+    }else{
+        _LabelText.text=[NSString stringWithFormat:@"%@%d",_LabelText.text,button.tag];
+    }
+       
+    }
 }
 
+-(IBAction)clearBtn
 @end
